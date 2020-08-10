3c1752dbe9220f74991419440333a10c018d378c
SongSugeun <songsk22366@naver.com>
2020-08-10 22:41:46 +0900

    SharedPreferencesの導入

---
+++ SONGPJ/YourPuppyDiary/app/src/main/java/com/example/your_puppy_diary/main/MySharedPreferences.kt
@@ -0,0 +1,30 @@
+package com.example.your_puppy_diary.main
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.example.your_puppy_diary.main.data.CalendarModel
+import com.google.gson.Gson
+
+class MySharedPreferences(context: Context) {
+    companion object {
+        private const val CALENDAR_DATE = "calendarDate"
+        private const val CALENDAR_MODEL = "calendarModel"
+    }
+
+    private val preferences: SharedPreferences = context.getSharedPreferences(CALENDAR_DATE, 0)
+
+    var myCalendar: CalendarModel?
+        set(calendarModel: CalendarModel?) {
+            val setGson: String = Gson().let {
+                it.toJson(myCalendar)
+            }
+            preferences.edit()
+                .putString(CALENDAR_MODEL, setGson)
+                .apply()
+        }
+        get() {
+            val getGson = Gson()
+            val calendarModel = preferences.getString(CALENDAR_MODEL, "")
+            return getGson.fromJson(calendarModel, CalendarModel::class.java)
+        }
+}
